1207e576f95f6d1f952db5b9325a7185c52a73f7
Gustavo <mxgugaa@gmail.com>
2022-09-01 14:18:44 -0300

    Componentização

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Container, Content } from "./styles";
+
+export function Header() { 
+    return(
+        <>
+            <header>
+                <Container />
+                <Content />
+            </header>
+        </>
+    )
+}
