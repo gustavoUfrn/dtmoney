4773d2c4b49cc1407e4ae9f5ba02e3e4174c179c
Gustavo <mxgugaa@gmail.com>
2022-09-15 17:56:42 -0300

    Funcionamento dos botões.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import Modal from 'react-modal';
+import { useState } from "react";
 
 interface NewTransactionModalProps {
 	isOpen: boolean;
@@ -10,34 +11,42 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+	const [type, setType] = useState('deposit');
+
 	return (
 		<Modal isOpen={isOpen}
 			onRequestClose={onRequestClose}
 			overlayClassName="react-modal-overlay"
 			className="react-modal-content"
-			>
+		>
 
-		<button type="button" 
-			onClick={onRequestClose} 
-			className="react-modal-close">
-				<img src={closeImg} alt="fechar"/>
-		</button>
+			<button type="button"
+				onClick={onRequestClose}
+				className="react-modal-close">
+				<img src={closeImg} alt="fechar" />
+			</button>
 
 			<Container>
 				<h2>Cadastrar transação</h2>
 
 				<input placeholder="Título"></input>
 				<input type="number" placeholder="Valor"></input>
-				
+
 				<TransactionTypeContainer>
-					<button type="button" >
+					<RadioBox type="button" 
+							onClick={() => { setType('deposit'); }}
+							isActive={type === 'deposit'}>
 						<img src={incomeImg} alt="Entrada" />
 						<span>Entrada</span>
-					</button>
-					<button type="button" >
+					</RadioBox>
+
+					<RadioBox type="button" 
+							onClick={() => { setType('withdraw'); }}
+							isActive={type === 'withdraw'}>
 						<img src={outcomeImg} alt="Saida" />
 						<span>Saida</span>
-					</button>
+					</RadioBox>
 				</TransactionTypeContainer>
 
 				<input placeholder="Categoria"></input>
